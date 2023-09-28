ada111c99da48194270bfeabf951fbb2249b6614
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2023-09-28 21:42:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Add event listener to change the background color on click
 document.addEventListener('click', function() {
     var randomColor = getRandomColor();
     document.body.style.backgroundColor = randomColor;
 });
 
-// Function to generate a random color
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -14,7 +12,6 @@ function getRandomColor() {
     return color;
 }
 
-// Add animation to the heading
 var heading = document.querySelector('h1');
 heading.addEventListener('mouseover', function() {
     heading.classList.add('animated');
